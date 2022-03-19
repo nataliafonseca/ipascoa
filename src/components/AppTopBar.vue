3f1e943eef92b8bdcef2ab0212d3059d2321b506
Natália Fonseca <nataliabf@outlook.com>
2022-03-19 13:31:51 -0300

    funcionalidades de registro e login

--- src/components/AppTopBar.vue
+++ src/components/AppTopBar.vue
@@ -1,36 +1,38 @@
 <template>
-    <div class="app-top-bar">
-         <v-app-bar color="green darken-4" dense dark>
+  <div class="app-top-bar">
+    <v-app-bar color="green darken-4" dense dark>
       <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
 
       <v-toolbar-title class="pl-0">iPascoa</v-toolbar-title>
 
       <v-spacer></v-spacer>
 
-     <!-- <v-btn icon>
+      <!-- <v-btn icon>
         <v-icon>mdi-heart</v-icon>
       </v-btn> -->
 
       <!-- <v-btn icon>
         <v-icon>mdi-magnify</v-icon>
       </v-btn> -->
+      <v-btn icon @click="$store.dispatch('logout')">
+        <v-icon>mdi-logout</v-icon>
+      </v-btn>
 
       <v-menu left bottom></v-menu>
-
-      </v-app-bar>
-      <v-navigation-drawer v-model="drawer" absolute temporary >
+    </v-app-bar>
+    <v-navigation-drawer v-model="drawer" absolute temporary>
       <v-list nav dense>
         <v-list-item-group
           v-model="group"
           active-class="deep-purple--text text--accent-4"
         >
-            <router-link to="/">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-home</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Inicio</v-list-item-title>
-          </v-list-item>
+          <router-link to="/">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-home</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Inicio</v-list-item-title>
+            </v-list-item>
           </router-link>
 
           <!-- <v-router-link to="/"> -->
@@ -42,7 +44,7 @@
           </v-list-item>
           <!-- </v-router-link> -->
 
-           <!-- <v-router-link to="/"> -->
+          <!-- <v-router-link to="/"> -->
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-store</v-icon>
@@ -51,7 +53,7 @@
           </v-list-item>
           <!-- </v-router-link> -->
 
-           <!-- <v-router-link to="/"> -->
+          <!-- <v-router-link to="/"> -->
           <v-list-item>
             <v-list-item-icon>
               <v-icon>mdi-store</v-icon>
@@ -60,41 +62,46 @@
           </v-list-item>
           <!-- </v-router-link> -->
 
-           <router-link to="/registro">
-          <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-account-plus</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Cadastro</v-list-item-title>
-          </v-list-item>
+          <router-link to="/registro">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account-plus</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Cadastro</v-list-item-title>
+            </v-list-item>
           </router-link>
 
-        <router-link to="/sobre">
-         <v-list-item>
-            <v-list-item-icon>
-              <v-icon>mdi-information</v-icon>
-            </v-list-item-icon>
-            <v-list-item-title>Sobre</v-list-item-title>
-          </v-list-item>
-        </router-link>
+          <router-link to="/login">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-account</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Login</v-list-item-title>
+            </v-list-item>
+          </router-link>
 
+          <router-link to="/sobre">
+            <v-list-item>
+              <v-list-item-icon>
+                <v-icon>mdi-information</v-icon>
+              </v-list-item-icon>
+              <v-list-item-title>Sobre</v-list-item-title>
+            </v-list-item>
+          </router-link>
         </v-list-item-group>
       </v-list>
     </v-navigation-drawer>
-    
-    </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AppTopBar',
-    data: () => ({
-      drawer: false,
-      group: null,
-    }),
-}
+  name: "AppTopBar",
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
 
-<style scoped>
-
-</style>
+<style scoped></style>
--- src/main.js
+++ src/main.js
@@ -1,12 +1,18 @@
-import Vue from 'vue'
-import App from './App.vue'
-import vuetify from './plugins/vuetify'
-import router from './router'
+import Vue from "vue";
+import VueToastify from "vue-toastify";
 
-Vue.config.productionTip = false
+import App from "./App.vue";
+import vuetify from "./plugins/vuetify";
+import router from "./router";
+import store from "./store";
+
+Vue.config.productionTip = false;
+
+Vue.use(VueToastify);
 
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
-}).$mount('#app')
+  store,
+  render: (h) => h(App),
+}).$mount("#app");
