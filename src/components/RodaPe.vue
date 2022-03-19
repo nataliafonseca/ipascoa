d7858b0dcad2b81aaa6e3c89b2dc2684c028fade
Sara Barros <sarasgnt@gmail.com>
2022-03-19 17:01:46 -0300

    formatando texto

--- src/components/RodaPe.vue
+++ src/components/RodaPe.vue
@@ -11,8 +11,8 @@
       </v-card-text>
 
       <v-card-text class="white--text pt-0">
-        Somos uma platafoma que transfroma a sua pascóa conectando você as
-        melhores produções artesanias de ovos de páscoa com incrível rapidez e
+        Somos uma platafoma que transfroma a sua páscoa conectando você as
+        melhores produções artesanais de ovos de páscoa com incrível rapidez e
         facilidade.
       </v-card-text>
 
@@ -20,7 +20,7 @@
 
       <v-card-text class="white--textD">
         <v-icon> mdi-egg-easter </v-icon>
-        All coprights reserved Ipascoa © 2022
+        All copyrights reserved Ipascoa © 2022
         <v-icon> mdi-egg-easter </v-icon>
       </v-card-text>
     </v-card>
