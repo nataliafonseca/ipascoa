bc32b46d6dde048611a3303841bdb87967ae6a4e
Natália Fonseca <nataliabf@outlook.com>
2022-03-19 15:06:34 -0300

    estilo em pagina da loja

--- src/views/PaginaLoja.vue
+++ src/views/PaginaLoja.vue
@@ -1,20 +1,60 @@
 <template>
   <v-container>
-    <h2>{{ informacao.nome }}</h2>
-    <h3 class="text-h7 text-center mb-3 mt-5">
+    <h1 class="titulo text-center mb-1 mt-5">{{ informacao.nome }}</h1>
+    <h3 class="text-h7 text-center mb-3">
       {{ informacao.endereco }}
     </h3>
 
     <h2 class="text-h5 text-center mb-3 mt-5">Nossos Ovos</h2>
 
-    <ul>
-      <li v-for="produto in produtos" :key="produto.id">
-        <h2>{{ produto.nome }}</h2>
-        <h4>{{ produto.sabor }}</h4>
-        <h4>{{ produto.preco | formatarPreco }}</h4>
-        <img :src="produto.imagem" />
-      </li>
-    </ul>
+    <div v-for="produto in produtos" :key="produto.id">
+      <v-card max-width="375" class="mx-auto mb-6">
+        <v-img :src="produto.imagem" height="300px" dark>
+          <v-row class="fill-height">
+            <v-card-title>
+              <v-btn dark icon>
+                <v-icon>mdi-chevron-left</v-icon>
+              </v-btn>
+
+              <v-spacer></v-spacer>
+
+              <v-btn dark icon class="mr-4">
+                <v-icon>mdi-pencil</v-icon>
+              </v-btn>
+
+              <v-btn dark icon>
+                <v-icon>mdi-dots-vertical</v-icon>
+              </v-btn>
+            </v-card-title>
+
+            <v-spacer></v-spacer>
+
+            <v-card-title class="white--text pl-12 pt-12"> </v-card-title>
+          </v-row>
+        </v-img>
+
+        <v-list two-line>
+          <v-list-item>
+            <h4 class="text-h4 pl-12 pt-12">{{ produto.nome }}</h4>
+          </v-list-item>
+
+          <v-divider inset></v-divider>
+
+          <v-divider inset></v-divider>
+
+          <v-list-item>
+            <v-list-item-content>
+              <v-list-item-title>{{ produto.sabor }}</v-list-item-title>
+
+              <v-list-item-subtitle
+                >Preço:
+                {{ produto.preco | formatarPreco }}</v-list-item-subtitle
+              >
+            </v-list-item-content>
+          </v-list-item>
+        </v-list>
+      </v-card>
+    </div>
   </v-container>
 </template>
 
