415ce0931b4153f11b967f9d4d889e4ed5ac1bab
eltonracarvalho <eltonracarvalho@gmail.com>
2022-03-19 14:17:35 -0300

    PaginaLoja

--- src/views/PaginaLoja.vue
+++ src/views/PaginaLoja.vue
@@ -1,13 +1,22 @@
 <template>
   <v-container>
-    <h2
-      tr
-      v-for="nome of lojaLista"
-      :key="nome.nome"
-      class="text-h5 text-center mb-3 mt-5"
-    >
-      {{ nome }}
-    </h2>
+    <h2>Posto Renoir</h2>
+    <h3 class="text-h7 text-center mb-3 mt-5">
+      Rua Taighum LLL, 566 - Paisagem Loud - Manaus - AM
+    </h3>
+
+    <h2 class="text-h5 text-center mb-3 mt-5">Nossos Ovos</h2>
+
+    <ul>
+      <li>
+        <h2>Ovo delícias</h2>
+        <h4>simples com chocolate ao leite</h4>
+        <h4>16,80</h4>
+        <img
+          src="https://todateen.com.br/wp-content/uploads/posts-astrallove/papobff/ovo-de-pascoa-ao-leite.jpg"
+        />
+      </li>
+    </ul>
   </v-container>
 </template>
 
